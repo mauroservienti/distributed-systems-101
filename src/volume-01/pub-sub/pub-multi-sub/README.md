f29b180e64dc10de35a35179ad8c3c2073f0ad7b
mauroservienti <mauro.servienti@gmail.com>
2021-05-05 13:36:33 +0200

    multiple subscribers demo

---
+++ src/volume-01/pub-sub/pub-multi-sub/README.md
@@ -0,0 +1,22 @@
+# Demo 2 - Request/Response
+
+> Note:
+>
+> - for this demos the order in which endpoints are executed is important
+> - folder paths are case sensitive
+
+## Start the Sales endpoint
+
+Using the Visual Studio Code terminal window (if none is present create a new one), navigate from the root of the repository to the `src/volume-01/pub-sub/basic-pub-sub/sales` folder. Build the Sales project using the `dotnet build` command. Once the project is build run it using the `dotnet run` command.
+
+## Start the Billing endpoint
+
+Using a new Visual Studio Code terminal window, navigate from the root of the repository to the `src/volume-01/pub-sub/basic-pub-sub/billing` folder. Build the Billing project using the `dotnet build` command. Once the project is build run it using the `dotnet run` command.
+
+## Start the Warehouse endpoint
+
+Using a new Visual Studio Code terminal window, navigate from the root of the repository to the `src/volume-01/pub-sub/basic-pub-sub/warehouse` folder. Build the Warehouse project using the `dotnet build` command. Once the project is build run it using the `dotnet run` command.
+
+## Start the Website endpoint
+
+Using a new Visual Studio Code terminal window, navigate from the root of the repository to the `src/volume-01/pub-sub/basic-pub-sub/website` folder. Build the Sales project using the `dotnet build` command. Once the project is build run it using the `dotnet run` command.
---
+++ src/volume-01/pub-sub/pub-multi-sub/sales/Program.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Sales
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ConfirmSelect();
+                channel.QueueDeclare(queue: "sales",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.ExchangeDeclare(exchange: "order.accepted",
+                                        durable: true,
+                                        type: "topic");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var receivedBody = ea.Body.ToArray();
+                    var receivedMessage = Encoding.UTF8.GetString(receivedBody);
+                    var receivedProps = ea.BasicProperties;
+                    Console.WriteLine($"Received {receivedMessage} with correlation ID {receivedProps.CorrelationId}");
+
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = receivedProps.CorrelationId;
+
+                    string replyMessage = $"Order {receivedProps.CorrelationId} on its way...";
+                    var replyBody = Encoding.UTF8.GetBytes(replyMessage);
+
+                    channel.BasicPublish(exchange: "",
+                                     routingKey: receivedProps.ReplyTo,
+                                     basicProperties: replyProps,
+                                     body: replyBody);
+                    channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 5));
+                
+                    Console.WriteLine($"Replied {replyMessage}");
+
+                    var eventProps = channel.CreateBasicProperties();
+                    eventProps.CorrelationId = receivedProps.CorrelationId;
+
+                    string eventMessage = $"Order {receivedProps.CorrelationId} accepted";
+                    var eventBody = Encoding.UTF8.GetBytes(eventMessage);
+
+                    channel.BasicPublish(exchange: "order.accepted",
+                                         routingKey: "order.accepted",
+                                         basicProperties: eventProps,
+                                         body: eventBody);
+                    channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 5));
+
+                    Console.WriteLine($"Published {eventMessage}");
+                };
+                channel.BasicConsume(queue: "sales",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Sales endpoint running.");
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
