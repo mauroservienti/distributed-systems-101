f9ed10149677a270d6f3df68c34778d7bf1f315c
mauroservienti <mauro.servienti@gmail.com>
2021-05-06 15:49:13 +0200

    Copy lesson 3

---
+++ src/volume-01/arch/cmd-events-orch/website/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using Sales.Messages.Commands;
+
+namespace Website
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var config = new EndpointConfiguration("website");
+            config.EnableInstallers();
+
+            var conventions = config.Conventions();
+            conventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"));
+            conventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith("Events"));
+            conventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith("Messages"));
+
+            var transport = config.UseTransport<RabbitMQTransport>();
+            transport.ConnectionString("host=localhost");
+            transport.UseConventionalRoutingTopology();
+
+            var routing = transport.Routing();
+            routing.RouteToEndpoint(typeof(PlaceOrder).Assembly, "sales");
+
+            var endpoint = await Endpoint.Start(config);
+
+            Console.WriteLine(" NServiceBus Website endpoint running.");
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            await endpoint.Stop();
+        }
+    }
+}
