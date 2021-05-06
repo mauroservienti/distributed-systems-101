05bec711744c47ee46c75b0f4d9d08da04eb3256
mauroservienti <mauro.servienti@gmail.com>
2021-05-06 15:49:24 +0200

    Add shipping skeleton

---
+++ src/volume-01/arch/cmd-events-orch/shipping/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Shipping
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var config = new EndpointConfiguration("shipping");
+            config.EnableInstallers();
+
+            var conventions = config.Conventions();
+            conventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"));
+            conventions.DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith("Events"));
+            conventions.DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith("Messages"));
+
+            var transport = config.UseTransport<RabbitMQTransport>();
+            transport.ConnectionString("host=localhost");
+            transport.UseConventionalRoutingTopology();
+
+            var endpoint = await Endpoint.Start(config);
+
+            Console.WriteLine(" NServiceBus Shipping endpoint running.");
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            await endpoint.Stop();
+        }
+    }
+}
