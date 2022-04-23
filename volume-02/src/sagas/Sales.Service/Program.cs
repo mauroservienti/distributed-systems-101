e023340ea895c6d7cccd689042f1fcece1ad364c
Mauro Servienti <mauro.servienti@gmail.com>
2022-04-23 09:12:47 +0200

    it's never null

--- volume-02/src/sagas/Sales.Service/Program.cs
+++ volume-02/src/sagas/Sales.Service/Program.cs
@@ -9,7 +9,7 @@ namespace Sales.Service
         static async Task Main(string[] args)
         {
             var serviceName = typeof(Program).Namespace;
-            Console.Title = serviceName;
+            Console.Title = serviceName!;
 
             var config = new EndpointConfiguration(serviceName);
             config.ApplyCommonConfigurationWithPersistence(@"Host=localhost;Username=db_user;Password=P@ssw0rd;Database=sales_database");
--- volume-02/src/sagas/Warehouse.Service/Program.cs
+++ volume-02/src/sagas/Warehouse.Service/Program.cs
@@ -9,7 +9,7 @@ namespace Warehouse.Service
         static async Task Main(string[] args)
         {
             var serviceName = typeof(Program).Namespace;
-            Console.Title = serviceName;
+            Console.Title = serviceName!;
 
             var config = new EndpointConfiguration(serviceName);
             config.ApplyCommonConfigurationWithPersistence(@"Host=localhost;Username=db_user;Password=P@ssw0rd;Database=warehouse_database");
