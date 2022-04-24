72f4f1dd4e6d3eb5b5b7b085e8b5477977668517
Mauro Servienti <mauro.servienti@gmail.com>
2022-04-24 09:22:18 +0200

    Remove custom checks configuration

--- vol-2/lesson-4/src/Shipping.Service/Program.cs
+++ vol-2/lesson-4/src/Shipping.Service/Program.cs
@@ -14,18 +14,6 @@ namespace Shipping.Service
             var config = new EndpointConfiguration(serviceName);
             config.ApplyCommonConfigurationWithPersistence(@"Host=localhost;Port=7432;Username=db_user;Password=P@ssw0rd;Database=shipping_database");
 
-            config.ReportCustomChecksTo(serviceControlQueue: "Particular.ServiceControl");
-            var recoverabilityConfig = config.Recoverability();
-            recoverabilityConfig.Immediate(immediate => 
-            {
-                immediate.NumberOfRetries(1);
-            });
-            recoverabilityConfig.Delayed(delayed =>
-            {
-                delayed.NumberOfRetries(1);
-                delayed.TimeIncrease(TimeSpan.FromSeconds(5));
-            });
-
             var endpointInstance = await Endpoint.Start(config);
 
             Console.WriteLine($"{serviceName} started. Press any key to stop.");
