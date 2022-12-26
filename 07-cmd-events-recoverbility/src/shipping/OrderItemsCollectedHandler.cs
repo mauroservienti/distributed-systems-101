99f6477ab9cd1473df9e925c0267045f4da036f2
Mauro Servienti <mauro.servienti@gmail.com>
2022-12-26 22:13:46 +0100

    better error raising strategy

--- 07-cmd-events-recoverbility/src/shipping/OrderItemsCollectedHandler.cs
+++ 07-cmd-events-recoverbility/src/shipping/OrderItemsCollectedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Npgsql;
 using NServiceBus;
 using Warehouse.Events;
@@ -7,14 +8,25 @@ namespace Shipping
 {
     public class OrderItemsCollectedHandler : IHandleMessages<OrderItemsCollected>
     {
-        public Task Handle(OrderItemsCollected message, IMessageHandlerContext context)
+        public async Task Handle(OrderItemsCollected message, IMessageHandlerContext context)
         {
             using var con = new NpgsqlConnection("Host=localhost;Username=db_user;Password=P@ssw0rd;Database=shipping_database");
             con.Open();
 
             var sql = $"UPDATE shipments SET ready = 'true' WHERE order_id = '{message.OrderId}'";
             using var cmd = new NpgsqlCommand(sql, con);
-            return cmd.ExecuteNonQueryAsync();
+            var affectedRecords = await cmd.ExecuteNonQueryAsync();
+
+            var rnd = new Random(DateTime.Now.Millisecond);
+            var delay = rnd.Next(1000, 3000);
+            await Task.Delay(delay);
+
+            if(affectedRecords == 0)
+            {
+                throw new Exception("Cannot find any record to update.");
+            }
+
+            Console.WriteLine("Shipment details updated");
             
             //probably publish a ShipmentReady event.
         }
