0147b856a39512968ba54b9888be24edb33adfea
Mauro Servienti <mauro.servienti@gmail.com>
2022-12-26 10:39:31 +0100

    Make targets simpler (#52)

--- targets/Program.cs
+++ targets/Program.cs
@@ -3,16 +3,13 @@ using System.Threading.Tasks;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
-internal class Program
+class Program
 {
     public static async Task Main(string[] args)
     {
-        var sdk = new DotnetSdkManager();
-        var dotnet = await sdk.GetDotnetCliPath();
-
         Target("default",
             Directory.EnumerateFiles(".", "*.sln", SearchOption.AllDirectories),
-            solution => Run(dotnet, $"build \"{solution}\" --configuration Release"));
+            solution => Run("dotnet", $"build \"{solution}\" --configuration Release"));
 
         await RunTargetsAndExitAsync(args);
     }
