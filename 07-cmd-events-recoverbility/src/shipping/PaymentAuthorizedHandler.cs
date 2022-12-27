25aa78f8e804062b68208794b8bbed3896ac31e6
Mauro Servienti <mauro.servienti@gmail.com>
2022-12-27 18:43:42 +0100

    Improved Dev containers support (#54)
    * Required build & run support
    
    * Demo 01: Dev container support
    
    * Demo 12: simpler dev container support
    
    * typo
    
    * it's not a real web app
    
    * Demo 02: build support
    
    * Demo 02: dev container support
    
    * Remove copied dev container support
    
    * Demo 3: build support
    
    * Demo 3: dev support
    
    * no need for PostgreSQL
    
    * Demo 4: build support
    
    * Demo 4: dev support
    
    * Demo 5: build support
    
    * Demo 5: dev support
    
    * Demo 6: build support
    
    * Demo 6: dev support
    
    * Demo 7: build support
    
    * Demo 7: dev support
    
    * better error raising strategy
    
    * Remove ports mapping
    
    * Demo 8: build support
    
    * Demo 8: dev support
    
    * Some logging
    
    * No need to define ports mappings in docker compose
    
    * Demo 9: build support
    
    * Demo 9: dev support
    
    * Int instead of long, some more logging
    
    * reame compounds
    
    * Simplify dev support
    
    * Rename compounds
    
    * Better dev containers support
    
    * Rename compounds
    
    * Remove not required ports

--- 07-cmd-events-recoverbility/src/shipping/PaymentAuthorizedHandler.cs
+++ 07-cmd-events-recoverbility/src/shipping/PaymentAuthorizedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Billing.Events;
 using Npgsql;
 using NServiceBus;
@@ -7,14 +8,20 @@ namespace Shipping
 {
     public class PaymentAuthorizedHandler : IHandleMessages<PaymentAuthorized>
     {
-        public Task Handle(PaymentAuthorized message, IMessageHandlerContext context)
+        public async Task Handle(PaymentAuthorized message, IMessageHandlerContext context)
         {
             using var con = new NpgsqlConnection("Host=localhost;Username=db_user;Password=P@ssw0rd;Database=shipping_database");
             con.Open();
 
             var sql = $"INSERT INTO shipments(order_id, ready) VALUES('{message.OrderId}',false)";
             using var cmd = new NpgsqlCommand(sql, con);
-            return cmd.ExecuteNonQueryAsync();
+            await cmd.ExecuteNonQueryAsync();
+
+            var rnd = new Random(DateTime.Now.Millisecond);
+            var delay = rnd.Next(1000, 3000);
+            await Task.Delay(delay);
+
+            Console.WriteLine("Shipment details created");
         }
     }
 }
--- 09-timeouts/src/shipping/ShippingPolicy.cs
+++ 09-timeouts/src/shipping/ShippingPolicy.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Billing.Events;
 using NServiceBus;
 using Shipping.Events;
@@ -19,22 +20,26 @@ namespace Shipping
 
         public Task Handle(PaymentAuthorized message, IMessageHandlerContext context)
         {
+            Console.WriteLine($"Handling {typeof(PaymentAuthorized)} for order {message.OrderId}.");
             Data.IsPaymentAuthorized = true;
-            return VerifyStatus(context);
+            return VerifyStatus(message.OrderId, context);
         }
 
         public Task Handle(OrderItemsCollected message, IMessageHandlerContext context)
         {
+            Console.WriteLine($"Handling {typeof(OrderItemsCollected)} for order {message.OrderId}.");
             Data.AreOrderItemsCollected = true;
-            return VerifyStatus(context);
+            return VerifyStatus(message.OrderId, context);
         }
         
-        private Task VerifyStatus(IMessageHandlerContext context)
+        private Task VerifyStatus(string orderId, IMessageHandlerContext context)
         {
             if (Data.IsPaymentAuthorized && Data.AreOrderItemsCollected)
             {
-                //We wanna see data in the db during the demos
-                //MarkAsComplete();
+                Console.WriteLine($"Order {orderId} is ready to be shipped.");
+                
+                // Comment the MarkAsComplete invocation to look at data in the db during the demo
+                MarkAsComplete();
                 return context.Publish(new ShipmentReady() {OrderId = Data.OrderId});
             }
 
