858e4209b91497ea737fdee8e8be4189da21d456
Mauro Servienti <mauro.servienti@gmail.com>
2024-11-06 10:11:46 +0100

    Update to async version

--- 01-basic-send/src/website/Program.cs
+++ 01-basic-send/src/website/Program.cs
@@ -1,33 +1,29 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Website
 {
     class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.ConfirmSelect();
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync(new CreateChannelOptions(true, true));
 
-                Console.WriteLine(" Press [enter] to send a message.");
-                Console.ReadLine();
+            Console.WriteLine(" Press [enter] to send a message.");
+            Console.ReadLine();
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "sales",
-                                     basicProperties: null,
-                                     body: body);
-                channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 5));
-                
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
+            const string message = "Hello World!";
+            await channel.BasicPublishAsync(
+                "", 
+                "sales", 
+                true, 
+                Encoding.UTF8.GetBytes(message));
+            
+            Console.WriteLine(" [x] Sent {0}", message);
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
